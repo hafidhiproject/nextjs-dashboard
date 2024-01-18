32ddac1031e769d49e9e6cb90e3282b36b9c6bb3
Azis Setyawan Nugroho <azis.nugroho@kemenkeu.go.id>
2024-01-18 18:38:18 +0700

    Inisialisasi proyek

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,11 @@
+import { Poppins, Lusitana } from 'next/font/google';
+
+export const fontInter = Poppins({
+  subsets: ['latin'],
+  weight: '500',
+});
+
+export const fontLusitana = Lusitana({
+  subsets: ['latin'],
+  weight: '400',
+});
